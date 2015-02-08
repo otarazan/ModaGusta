70fcf41b453956f8bc7eca9fcc59abe352508d0f
olcay <o.tarazan@tum.de>
2015-02-08 22:41:20 +0100

    unused code removed

--- modagusta/www/js/app.js
+++ modagusta/www/js/app.js
@@ -4,6 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
@@ -50,4 +51,35 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/browse');
-});
+}).controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+        // Form data for the login modal
+        $scope.loginData = {};
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+          scope: $scope
+        }).then(function(modal) {
+          $scope.modal = modal;
+        });
+
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function() {
+          $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function() {
+          $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function() {
+          console.log('Doing login', $scope.loginData);
+
+          // Simulate a login delay. Remove this and replace with your login
+          // code if using a login system
+          $timeout(function() {
+            $scope.closeLogin();
+          }, 1000);
+        };
+      });
