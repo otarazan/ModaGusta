fde24f172d1aeb08370cd25c69f7c3c39134799f
olcay <o.tarazan@tum.de>
2015-02-15 04:29:47 +0100

    basic format fix

--- modagusta/www/app/components/menu/menu.js
+++ modagusta/www/app/components/menu/menu.js
@@ -10,6 +10,75 @@ myApp.controller('MenuCtrl', function($scope, $http, $ionicPopup, $timeout, $roo
 
 
 
+    $scope.validateEmail = function(email) {
+
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+
+        } //validateEmail
+
+
+    $scope.btnRemoveFromWishList = function(index) {
+
+        $scope.wishList.splice($scope.wishList.indexOf(index), 1);
+        localStorageService.set('wishList', $scope.wishList);
+
+    };
+
+
+    $scope.clearWishList = function() {
+
+
+        localStorageService.clearAll();
+        $scope.wishList = null;
+        $rootScope.wishList = null;
+
+    }; //clearWishlist
+
+
+
+    $scope.sendWishListMail = function() {
+
+        $scope.data = {}
+        $scope.data.email="olcaytarazan@gmail.com";
+        var myPopup = $ionicPopup.show({
+            template: '<input type="text" ng-model="data.email">',
+            title: 'Please enter your E-mail.',
+            scope: $scope,
+            buttons: [{
+                text: 'Cancel'
+            }, {
+                text: '<b>Send</b>',
+                type: 'button-positive',
+                onTap: function(e) {
+                    if (!$scope.data.email) {
+                        //don't allow the user to close unless he enters wifi password
+                        e.preventDefault();
+                    } else {
+
+                        if ($scope.validateEmail($scope.data.email)) {
+                            $http.post(server+"sendWishListMail", {wishList:$scope.wishList}).
+                                  success(function(data, status, headers, config) {
+                                        console.log("ok");
+                                  }).
+                                  error(function(data, status, headers, config) {
+                                        console.log("fail");
+                                  });
+                        } else {
+                            alert("Please enter a valid email... ");
+                            $scope.sendWishListMail();
+                        }
+
+                        return $scope.data.email;
+                    }
+                }
+            }, ]
+        });
+
+
+    }; //sendWishlistMail
+
+
 
     var server="http://localhost:3000/";
 
@@ -22,78 +91,6 @@ myApp.controller('MenuCtrl', function($scope, $http, $ionicPopup, $timeout, $roo
       } else {
           $scope.wishList = localStorageService.get('wishList');
       }
-
-
-
-        $scope.sendWishListMail = function() {
-
-            $scope.data = {}
-            $scope.data.email="olcaytarazan@gmail.com";
-            var myPopup = $ionicPopup.show({
-                template: '<input type="text" ng-model="data.email">',
-                title: 'Please enter your E-mail.',
-                scope: $scope,
-                buttons: [{
-                    text: 'Cancel'
-                }, {
-                    text: '<b>Send</b>',
-                    type: 'button-positive',
-                    onTap: function(e) {
-                        if (!$scope.data.email) {
-                            //don't allow the user to close unless he enters wifi password
-                            e.preventDefault();
-                        } else {
-
-                            if ($scope.validateEmail($scope.data.email)) {
-                                $http.post(server+"sendWishListMail", {wishList:$scope.wishList}).
-                                      success(function(data, status, headers, config) {
-                                            console.log("ok");
-                                      }).
-                                      error(function(data, status, headers, config) {
-                                            console.log("fail");
-                                      });
-                            } else {
-                                alert("Please enter a valid email... ");
-                                $scope.sendWishListMail();
-                            }
-
-                            return $scope.data.email;
-                        }
-                    }
-                }, ]
-            });
-
-
-        }; //sendWishlistMail
-
-
-
-
-        $scope.validateEmail = function(email) {
-
-                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                return re.test(email);
-
-            } //validateEmail
-
-
-        $scope.btnRemoveFromWishList = function(index) {
-
-            $scope.wishList.splice($scope.wishList.indexOf(index), 1);
-            localStorageService.set('wishList', $scope.wishList);
-
-        };
-
-
-        $scope.clearWishList = function() {
-
-
-            localStorageService.clearAll();
-            $scope.wishList = null;
-            $rootScope.wishList = null;
-
-        }; //clearWishlist
-
 
     });
 
