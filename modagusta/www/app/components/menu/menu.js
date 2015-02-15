cac24c215fb632a2a200daa25a336d1c5d9bcd78
olcay <o.tarazan@tum.de>
2015-02-15 03:23:19 +0100

    mailing not done yet. Testing your changes

--- modagusta/www/app/components/menu/menu.js
+++ modagusta/www/app/components/menu/menu.js
@@ -1,4 +1,4 @@
-myApp.controller('MenuCtrl',  function($scope,$http, $ionicPopup,$timeout, $rootScope,$ionicSideMenuDelegate,localStorageService){
+myApp.controller('MenuCtrl', function($scope, $http, $ionicPopup, $timeout, $rootScope, $ionicSideMenuDelegate, localStorageService) {
 
 
 
@@ -11,90 +11,90 @@ myApp.controller('MenuCtrl',  function($scope,$http, $ionicPopup,$timeout, $root
 
 
 
-var server="http://localhost:3000/";
+    var server="http://localhost:3000/";
 
- $scope.$watch(function() {
-   return $ionicSideMenuDelegate.isOpenRight();
- }, function(value) {
+    $scope.$watch(function() {
+        return $ionicSideMenuDelegate.isOpenRight();
+    }, function(value) {
 
-  if ($rootScope.wishList == null ){
-         $rootScope.wishList = [];
-  }else{
-         $scope.wishList = localStorageService.get('wishList');
-  }
-
-
-
-$scope.sendWishListMail = function(){
-
-$scope.data = {}
-
-var myPopup = $ionicPopup.show({
-     template: '<input type="normal" ng-model="data.email">',
-     title: 'Please enter your E-mail.',
-     scope: $scope,
-     buttons: [
-       { text: 'Cancel' },
-       {
-         text: '<b>Send</b>',
-         type: 'button-positive',
-         onTap: function(e) {
-           if (!$scope.data.email) {
-             //don't allow the user to close unless he enters wifi password
-             e.preventDefault();
-           } else {
-
-                    if($scope.validateEmail($scope.data.email))
-                    {
-
-                       window.location = server+"sendWishListMail";
+      if ($rootScope.wishList == null) {
+          $rootScope.wishList = [];
+      } else {
+          $scope.wishList = localStorageService.get('wishList');
+      }
 
 
-                    alert("WishList is sent to: "+$scope.data.email);
+
+        $scope.sendWishListMail = function() {
+
+            $scope.data = {}
+            $scope.data.email="olcaytarazan@gmail.com";
+            var myPopup = $ionicPopup.show({
+                template: '<input type="text" ng-model="data.email">',
+                title: 'Please enter your E-mail.',
+                scope: $scope,
+                buttons: [{
+                    text: 'Cancel'
+                }, {
+                    text: '<b>Send</b>',
+                    type: 'button-positive',
+                    onTap: function(e) {
+                        if (!$scope.data.email) {
+                            //don't allow the user to close unless he enters wifi password
+                            e.preventDefault();
+                        } else {
+
+                            if ($scope.validateEmail($scope.data.email)) {
+                                $http.post(server+"sendWishListMail", {wishList:$scope.wishList}).
+                                      success(function(data, status, headers, config) {
+                                            console.log("ok");
+                                      }).
+                                      error(function(data, status, headers, config) {
+                                            console.log("fail");
+                                      });
+                            } else {
+                                alert("Please enter a valid email... ");
+                                $scope.sendWishListMail();
+                            }
+
+                            return $scope.data.email;
+                        }
                     }
-                    else{
-                    alert("Please enter a valid email... "); $scope.sendWishListMail();
-                    }
-
-                                 return $scope.data.email;
-                               }
-                             }
-                           },
-                 ]
-         });
+                }, ]
+            });
 
 
-};//sendWishlistMail
+        }; //sendWishlistMail
 
 
 
 
-$scope.validateEmail = function(email){
+        $scope.validateEmail = function(email) {
 
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(email);
+                var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
 
-}//validateEmail
-
-
-$scope.btnRemoveFromWishList = function(index){
-
-$scope.wishList.splice($scope.wishList.indexOf(index), 1);
- localStorageService.set('wishList', $scope.wishList);
-
-};
+            } //validateEmail
 
 
-$scope.clearWishList = function(){
+        $scope.btnRemoveFromWishList = function(index) {
+
+            $scope.wishList.splice($scope.wishList.indexOf(index), 1);
+            localStorageService.set('wishList', $scope.wishList);
+
+        };
 
 
-localStorageService.clearAll();
-$scope.wishList=null;
-$rootScope.wishList = null;
-
-};//clearWishlist
+        $scope.clearWishList = function() {
 
 
- });
+            localStorageService.clearAll();
+            $scope.wishList = null;
+            $rootScope.wishList = null;
 
-   });
+        }; //clearWishlist
+
+
+    });
+
+});
--- server/app.js
+++ server/app.js
@@ -1,63 +1,96 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-res.header('Access-Control-Allow-Origin', "*");
-   res.send('server works');
-   
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
 });
 
-app.get('/test', function (req, res) {
-	res.header('Access-Control-Allow-Origin', "*");
+app.get('/', function(req, res) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.send('server works');
+
+});
+
+app.get('/test', function(req, res) {
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-   res.json([
-    {id:'1', image: 'img/pic2.png', title: 'So much grass #hippster'},
-    {id:'2', image: 'img/pic3.png', title: 'Way too much Sand, right?'},
-    {id:'3', image: 'img/pic4.png', title: 'Beautiful sky from wherever'},
-    {id:'1', image: 'img/pic2.png', title: 'So much grass #hippster'}
-    ]);
+    res.json([{
+        id: '1',
+        image: 'img/pic2.png',
+        title: 'So much grass #hippster'
+    }, {
+        id: '2',
+        image: 'img/pic3.png',
+        title: 'Way too much Sand, right?'
+    }, {
+        id: '3',
+        image: 'img/pic4.png',
+        title: 'Beautiful sky from wherever'
+    }, {
+        id: '1',
+        image: 'img/pic2.png',
+        title: 'So much grass #hippster'
+    }]);
 });
 
-app.get('/buy', function (req, res) {
-    res.json("bought id="+req.query.id);
-
-
-});
-
-app.get('/sendWishListMail', function (req, res) {
-
-
-var nodemailer = require('nodemailer');
- var transporter = nodemailer.createTransport({
-                                service: 'gmail',
-                                auth: {
-                                    user: 'hus.alemdar@gmail.com',
-                                    pass: 'Naber123'
-                                }
-                            });
-                            transporter.sendMail({
-                                from: 'hus.alemdar@gmail.com',
-                                to: 'tarazansafak@gmail.com',
-                                subject: 'ModaGusta WishList'
-                            });
-
-    res.json("ok");
+app.get('/buy', function(req, res) {
+    res.json("bought id=" + req.query.id);
 
 
 });
 
-
-
-var server = app.listen(3000, function () {
-
-  var host = server.address().address
-  var port = server.address().port
+app.post('/sendWishListMail', function(req, res) {
 
 
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    req.on('data', function(data) {
+        var wishList=JSON.parse(data.toString());
+        console.log("Received mail data");
+        console.log(wishList.size);
+        console.log(wishList[0]);
+        //Prepare data in mail format
+        var mail = "Your Wish List:<br>";
+        for (i = 0; i < wishList.length; i++) {
+            console.log(mail);
+            mail += wishList[i].title + "<a href='#'><img height='60' width='60' src=''/></a><br>";
+        };
+        console.log(mail);
+      /*  var nodemailer = require('nodemailer');
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'hus.alemdar@gmail.com',
+                pass: 'Naber123'
+            }
+        });
+        transporter.sendMail({
+            from: 'hus.alemdar@gmail.com',
+            to: 'olcaytarazan@gmail.com',
+            subject: 'ModaGusta WishList',
+            html: mail
+        });
+*/
+        res.json("ok");
+
+
+    });
+
+});
+
+var server = app.listen(3000, function() {
+
+    var host = server.address().address
+    var port = server.address().port
+
+
+
+    console.log('Example app listening at http://%s:%s', host, port)
 
 })
-
-
